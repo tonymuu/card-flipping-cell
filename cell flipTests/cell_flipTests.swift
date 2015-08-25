b448a36d686fee99b12afbc897b7413406d95201
tonymu <tony.mu101999@gmail.com>
2015-08-25 18:07:52 -0500

    Initial Commit

---
+++ cell flipTests/cell_flipTests.swift
@@ -0,0 +1,36 @@
+//
+//  cell_flipTests.swift
+//  cell flipTests
+//
+//  Created by Mu Tong on 8/25/15.
+//  Copyright (c) 2015 Tony Mu. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class cell_flipTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
